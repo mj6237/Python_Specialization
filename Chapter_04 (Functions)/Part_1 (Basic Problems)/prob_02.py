bf1bf7aa98e3c5bcc810fff2c33e4ad5830e670a
Muhammad Jamshaid <128154619+mj6237@users.noreply.github.com>
2025-07-13 22:11:03 +0500

    adding two numbers using function

---
+++ Chapter_04 (Functions)/Part_1 (Basic Problems)/prob_02.py
@@ -0,0 +1,8 @@
+# Define a function add(a, b) that returns the sum of two numbers 
+
+def add(a,b) :
+    return a + b
+num1 = int(input("Enter first number :"))
+num2 = int(input("Enter second number :"))
+total =add(num1,num2)
+print(f"Sum is : {total}")
