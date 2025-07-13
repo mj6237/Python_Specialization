862c88b89f173f16bdfc69e1834c854a8bab9e76
Muhammad Jamshaid <128154619+mj6237@users.noreply.github.com>
2025-07-13 22:45:29 +0500

    square of a number

---
+++ Chapter_04 (Functions)/Part_1 (Basic Problems)/prob_03.py
@@ -0,0 +1,7 @@
+# Define a function square(n) that returns the square of a number 
+
+def square(n) :
+    return n**2
+n = int(input("Enter a number to get its square :"))
+sqr =square(n)
+print("Square of",n,"is : ",sqr)
