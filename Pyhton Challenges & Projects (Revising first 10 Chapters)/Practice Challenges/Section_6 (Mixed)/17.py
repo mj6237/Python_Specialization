b24c80ba8336d23bbcf1649803a7c338364c508c
Muhammad Jamshaid <128154619+mj6237@users.noreply.github.com>
2025-07-28 23:07:09 +0500

    create tuple of grades and demonstrate immutability

---
+++ Pyhton Challenges & Projects (Revising first 10 Chapters)/Practice Challenges/Section_6 (Mixed)/17.py
@@ -0,0 +1,10 @@
+'''
+1. Create a tuple student_grades containing 5 numerical grades (e.g., (85, 90, 78, 92, 88)). 
+2. Print the student_grades tuple. 
+3. Try to change the first grade in the tuple and observe the error.
+'''
+grades = (85, 90, 78, 92, 88)
+print(grades)
+
+#grades[0] = 99
+#print(grades)
