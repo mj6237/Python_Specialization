aa11b9a278b5a61c2b6f60668524c774dc7ad691
Muhammad Jamshaid <128154619+mj6237@users.noreply.github.com>
2025-07-28 20:20:54 +0500

    Add list of tuples for people data and extract second person's name

---
+++ Pyhton Challenges & Projects (Revising first 10 Chapters)/Practice Challenges/Section_6 (Mixed)/09.py
@@ -0,0 +1,11 @@
+'''
+1. Create a list named people_data. 
+2. Add three tuples to this list, each representing a person: ("Alice", 30), ("Bob", 24), ("Charlie", 35). 
+3. Print the entire people_data list. 
+4. Print the name of the second person in the list. 
+'''
+
+people_data = [("Alice", 30), ("Bob", 24), ("Charlie", 24)]
+print(people_data)
+x, y = people_data[1]
+print(x)
