46ef56e9cc9ea37a5f0db04cf04037556a96aa0f
Muhammad Jamshaid <128154619+mj6237@users.noreply.github.com>
2025-07-28 10:17:22 +0500

    Accessed first and last items from a list using indexing

---
+++ Pyhton Challenges & Projects (Revising first 10 Chapters)/Practice Challenges/Section_3 (Lists)/02.py
@@ -0,0 +1,11 @@
+'''
+What the Question Demands: 
+1. Use the fruits list from Problem 3.1. 
+2. Print the first fruit in the list. 
+3. Print the last fruit in the list. 
+'''
+
+fruits = ["Mango", "Apple", "Gava"]
+
+print(fruits[0])
+print(fruits[-1])
