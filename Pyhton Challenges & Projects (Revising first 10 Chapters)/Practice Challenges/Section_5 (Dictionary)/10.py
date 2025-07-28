2463901330dfc7545c3a284ba766069c40526d4a
Muhammad Jamshaid <128154619+mj6237@users.noreply.github.com>
2025-07-28 13:02:21 +0500

    printed total number of key-value pairs in dictionary using len()

---
+++ Pyhton Challenges & Projects (Revising first 10 Chapters)/Practice Challenges/Section_5 (Dictionary)/10.py
@@ -0,0 +1,11 @@
+'''
+1. Use the student dictionary (after Problem 5.4). 
+2. Print the total number of key-value pairs in the dictionary.
+'''
+student  = {
+    "name" : "Muhammad",
+    "age" : 24,
+    "major" : "Computer Science"
+}
+
+print(len(student))
