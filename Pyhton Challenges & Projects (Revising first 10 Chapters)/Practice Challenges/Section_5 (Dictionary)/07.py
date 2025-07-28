47f3f07aefa991dba64a1c0c397b9634e7ce2ee2
Muhammad Jamshaid <128154619+mj6237@users.noreply.github.com>
2025-07-28 12:54:58 +0500

    printed each key in dictionary using for loop and keys() method

---
+++ Pyhton Challenges & Projects (Revising first 10 Chapters)/Practice Challenges/Section_5 (Dictionary)/07.py
@@ -0,0 +1,13 @@
+'''
+1. Use the student dictionary (after Problem 5.4). 
+2. Use a for loop to iterate through all the keys in the dictionary. 
+3. Print each key. 
+'''
+student  = {
+    "name" : "Muhammad",
+    "age" : 24,
+    "major" : "Computer Science"
+}
+
+for k in student.keys() :
+    print(k)
