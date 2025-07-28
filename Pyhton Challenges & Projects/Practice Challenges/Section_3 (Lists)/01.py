ab41bea932c74a95e4a00c975871244c53e251c6
Muhammad Jamshaid <128154619+mj6237@users.noreply.github.com>
2025-07-28 10:13:08 +0500

    Created a fruits list and printed it

---
+++ Pyhton Challenges & Projects/Practice Challenges/Section_3 (Lists)/01.py
@@ -0,0 +1,8 @@
+'''
+What the Question Demands: 
+1. Create a list named fruits containing three of your favorite fruits (e.g., "apple", "banana", "orange"). 
+2. Print the entire fruits list.
+'''
+
+fruits = ["Mango", "Apple", "Gava"]
+print(fruits)
