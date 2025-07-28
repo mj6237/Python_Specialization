c213aa2f28f511c2ade1c04de332308a58dee30c
Muhammad Jamshaid <128154619+mj6237@users.noreply.github.com>
2025-07-28 10:19:43 +0500

    Appended a new fruit to the list and printed the updated list

---
+++ Pyhton Challenges & Projects (Revising first 10 Chapters)/Practice Challenges/Section_3 (Lists)/03.py
@@ -0,0 +1,11 @@
+'''
+What the Question Demands: 
+1. Use the fruits list from Problem 3.1. 
+2. Add a new fruit (e.g., "grape") to the end of the list. 
+3. Print the updated fruits list.
+'''
+
+fruits = ["Mango", "Apple", "Gava"]
+
+fruits.append("Grapes")
+print(fruits)
