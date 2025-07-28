715de9cbeae821f9f5095d050261aefbbbca6c5b
Muhammad Jamshaid <128154619+mj6237@users.noreply.github.com>
2025-07-28 10:43:21 +0500

    add basic function definition and call to print hello message

---
+++ Pyhton Challenges & Projects (Revising first 10 Chapters)/Practice Challenges/Section_4 (Functions)/01.py
@@ -0,0 +1,9 @@
+'''
+Define a function named say_hello that takes no parameters. 
+2. Inside the function, print "Hello from my first function!". 
+3. Call the say_hello function.
+'''
+def say_hello() :
+    print("Hello !")
+
+say_hello()
