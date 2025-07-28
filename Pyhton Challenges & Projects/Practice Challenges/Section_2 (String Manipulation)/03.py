b744aafb0324f030d58f3b4862e392ee62b0b90c
Muhammad Jamshaid <128154619+mj6237@users.noreply.github.com>
2025-07-28 08:27:47 +0500

    Implemented lowercase converter using .lower() method

---
+++ Pyhton Challenges & Projects/Practice Challenges/Section_2 (String Manipulation)/03.py
@@ -0,0 +1,8 @@
+'''
+What the Question Demands: 
+1. Ask the user to input a word or phrase. 
+2. Print the input string converted entirely to lowercase.
+'''
+
+user_input = input("Enter a word to convert into lowercase :")
+print(user_input.lower())
