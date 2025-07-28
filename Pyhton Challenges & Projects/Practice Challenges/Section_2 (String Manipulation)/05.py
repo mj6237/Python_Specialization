8a13dde51749d2b000bdd73c99a5294557f6eca6
Muhammad Jamshaid <128154619+mj6237@users.noreply.github.com>
2025-07-28 08:43:52 +0500

    Replaced all 'o' with 'X' in message using .replace()

---
+++ Pyhton Challenges & Projects/Practice Challenges/Section_2 (String Manipulation)/05.py
@@ -0,0 +1,8 @@
+'''
+program is your friend. 
+ What the Question Demands: 
+1. Define a string: message = "Hello, Python is fun!". 
+2. Replace all occurrences of the letter 'o' with the letter 'X'. 
+'''
+message = "Hello, Python is fun!"
+print(f"'{message}' = '{message.replace('o' , 'X')}'")
