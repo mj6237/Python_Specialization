a0ffb51914cf9ff3d580cdd0be7aeccc88876fff
Muhammad Jamshaid <128154619+mj6237@users.noreply.github.com>
2025-07-28 08:52:46 +0500

    Printed first character of user input using string indexing

---
+++ Pyhton Challenges & Projects/Practice Challenges/Section_2 (String Manipulation)/07.py
@@ -0,0 +1,10 @@
+'''
+What the Question Demands: 
+1. Ask the user to input a word. 
+2. Print the first character of the word. 
+'''
+user_input = input("Etner a word :")
+if user_input :
+    print(user_input[0])
+else :
+    print("You did't enter anything !")
