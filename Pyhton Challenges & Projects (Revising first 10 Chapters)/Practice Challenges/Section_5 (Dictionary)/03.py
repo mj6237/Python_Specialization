25bdc3ce48c009dda7942785ee637fd89408b390
Muhammad Jamshaid <128154619+mj6237@users.noreply.github.com>
2025-07-28 12:28:49 +0500

    added GPA key to student dictionary and printed updated dictionary

---
+++ Pyhton Challenges & Projects (Revising first 10 Chapters)/Practice Challenges/Section_5 (Dictionary)/03.py
@@ -0,0 +1,15 @@
+'''
+Use the student dictionary from Problem 5.1. 
+2. Add a new key-value pair: "gpa" with a value of 3.8. 
+3. Print the updated student dictionary. 
+'''
+
+student = {
+    "name" : "Muhammad",
+    "age" : 24,
+    "major" : "Computer Science"
+
+}
+
+student["cgpa"] = 3.5
+print(student)
