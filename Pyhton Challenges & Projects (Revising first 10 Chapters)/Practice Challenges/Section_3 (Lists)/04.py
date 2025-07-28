b8f6383d3ec62a03741274038d1bdd0ceb617f19
Muhammad Jamshaid <128154619+mj6237@users.noreply.github.com>
2025-07-28 10:22:41 +0500

    Removed a fruit from the list using remove()

---
+++ Pyhton Challenges & Projects (Revising first 10 Chapters)/Practice Challenges/Section_3 (Lists)/04.py
@@ -0,0 +1,11 @@
+'''
+What the Question Demands: 
+1. Use the fruits list (after Problem 3.3). 
+2. Remove one of the fruits (e.g., "banana") from the list. 
+3. Print the updated fruits list. 
+'''
+
+fruits = ["Mango", "Apple", "Gava", "Banana"]
+
+fruits.remove("Banana")
+print(fruits)
