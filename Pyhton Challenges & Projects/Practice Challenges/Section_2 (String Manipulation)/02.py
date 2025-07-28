f2eb5dfb35f8bd8b70e1c4673de66d0ea8f6337b
Muhammad Jamshaid <128154619+mj6237@users.noreply.github.com>
2025-07-28 08:25:18 +0500

    Created program to convert user input to uppercase using .upper(

---
+++ Pyhton Challenges & Projects/Practice Challenges/Section_2 (String Manipulation)/02.py
@@ -0,0 +1,7 @@
+'''
+What the Question Demands: 
+1. Ask the user to input a word or phrase. 
+2. Print the input string converted entirely to uppercase.
+'''
+user_input = input("Enter a word to get all in Capital letters :")
+print(user_input.upper())
