1a03e2eab0718322ca2548c08c2ef5a363ddfb72
Muhammad Jamshaid <128154619+mj6237@users.noreply.github.com>
2025-07-28 08:54:45 +0500

    Printed last character of user input with input validation

---
+++ Pyhton Challenges & Projects/Practice Challenges/Section_2 (String Manipulation)/08.py
@@ -0,0 +1,11 @@
+'''
+What the Question Demands: 
+1. Ask the user to input a word. 
+2. Print the last character of the word.
+'''
+
+user_input = input("Enter a word :")
+if user_input :
+    print(user_input[-1])
+else :
+    print("You Entered Nothing !")
