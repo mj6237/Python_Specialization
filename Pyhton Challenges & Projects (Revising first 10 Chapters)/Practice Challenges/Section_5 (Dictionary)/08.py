ae613c4c47b348b0eda8f00c8c9918bef5c6737d
Muhammad Jamshaid <128154619+mj6237@users.noreply.github.com>
2025-07-28 12:56:42 +0500

    printed each value in dictionary using for loop and values() method

---
+++ Pyhton Challenges & Projects (Revising first 10 Chapters)/Practice Challenges/Section_5 (Dictionary)/08.py
@@ -0,0 +1,14 @@
+'''
+1. Use the student dictionary (after Problem 5.4). 
+2. Use a for loop to iterate through all the values in the dictionary. 
+3. Print each value.
+'''
+
+student  = {
+    "name" : "Muhammad",
+    "age" : 24,
+    "major" : "Computer Science"
+}
+
+for v in student.values():
+    print(v)
