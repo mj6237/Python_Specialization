13560460be0e99a63eb8ad9a094f7820c79db585
Muhammad Jamshaid <128154619+mj6237@users.noreply.github.com>
2025-07-28 12:18:50 +0500

    create and print student dictionary with keys name, age, and major

---
+++ Pyhton Challenges & Projects (Revising first 10 Chapters)/Practice Challenges/Section_5 (Dictionary)/01.py
@@ -0,0 +1,15 @@
+'''
+Create a dictionary named student with the following key-value pairs: 
+ "name": "Alice" 
+ "age": 20 
+ "major": "Computer Science" 
+2. Print the entire student dictionary
+'''
+
+student = {
+    "name" : "Jamshaid",
+    "age" : 24,
+    "major" : "Computer Science"
+}
+
+print(student)
