3db3e6fbe3daffd1394474636565bfe1083d981a
Muhammad Jamshaid <128154619+mj6237@users.noreply.github.com>
2025-07-28 10:46:15 +0500

    add greet_person function with personalized input-based message

---
+++ Pyhton Challenges & Projects (Revising first 10 Chapters)/Practice Challenges/Section_4 (Functions)/02.py
@@ -0,0 +1,12 @@
+'''
+Define a function named greet_person that takes one parameter: name. 
+2. Inside the function, print a personalized greeting like "Hello, [name]!". 
+3. Call greet_person twice with different names.
+'''
+
+def greet_person(name) :
+    print(f"Asslam o Alaikum ! {name}")
+
+name = input("Enter your name :")
+
+greet_person(name)
