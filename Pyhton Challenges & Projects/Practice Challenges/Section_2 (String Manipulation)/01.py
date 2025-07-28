fa9019c1dac36ada9c0b58d9583c52ed0fd002c9
Muhammad Jamshaid <128154619+mj6237@users.noreply.github.com>
2025-07-28 08:19:44 +0500

    Sentence character count program using len()

---
+++ Pyhton Challenges & Projects/Practice Challenges/Section_2 (String Manipulation)/01.py
@@ -0,0 +1,7 @@
+'''
+What the Question Demands: 
+1. Ask the user to input a sentence. 
+2. Print the total number of characters in that sentence. 
+'''
+sentence = input("Enter a sentence to check the number of character :")
+print(f"The number of characters in your sentence are : {len(sentence)}")
