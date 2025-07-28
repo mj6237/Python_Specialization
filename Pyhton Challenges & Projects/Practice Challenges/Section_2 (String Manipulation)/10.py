8a1698ecf9e64726da07da628d2d6aeb776ffa3c
Muhammad Jamshaid <128154619+mj6237@users.noreply.github.com>
2025-07-28 10:07:07 +0500

    Joined a list of words into a single string using join()

---
+++ Pyhton Challenges & Projects/Practice Challenges/Section_2 (String Manipulation)/10.py
@@ -0,0 +1,9 @@
+'''
+Define a list of words: words = ["Hello", "World", "Python"]. 
+2. Join these words into a single string, separated by a space. 
+3. Print the resulting string. 
+'''
+
+words = ["Hello", "World", "Python"]
+result = " " .join(words)
+print(f"Origina words :{words}\n Joined words : = {result}")
