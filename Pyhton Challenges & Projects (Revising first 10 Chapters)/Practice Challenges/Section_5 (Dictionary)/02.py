f73bfefb6708d5b59ddbc98100badf47508cbd28
Muhammad Jamshaid <128154619+mj6237@users.noreply.github.com>
2025-07-28 12:26:24 +0500

    print name and major from student dictionary

---
+++ Pyhton Challenges & Projects (Revising first 10 Chapters)/Practice Challenges/Section_5 (Dictionary)/02.py
@@ -0,0 +1,15 @@
+''''
+1. Use the student dictionary from Problem 5.1. 
+2. Print the name of the student. 
+3. Print the major of the student. 
+'''
+
+student = {
+    "name" : "Muhammad",
+    "age" : 24,
+    "ajor" : "Computer Science"
+
+}
+
+print(student["name"])
+print(student["major"])
