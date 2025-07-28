21ff0e05095bf9abe09915d81407c87ee8ff0bc3
Muhammad Jamshaid <128154619+mj6237@users.noreply.github.com>
2025-07-28 12:31:22 +0500

    updated student age from 24 to 21 in dictionary

---
+++ Pyhton Challenges & Projects (Revising first 10 Chapters)/Practice Challenges/Section_5 (Dictionary)/04.py
@@ -0,0 +1,18 @@
+'''
+Use the student dictionary (after Problem 5.3). 
+2. Change the age of the student to 21. 
+3. Print the updated student dictionary.
+'''
+
+
+student = {
+    "name" : "Muhammad",
+    "age" : 24,
+    "major" : "Computer Science"
+
+}
+
+print(student)
+student["age"] = 21
+
+print(student)
