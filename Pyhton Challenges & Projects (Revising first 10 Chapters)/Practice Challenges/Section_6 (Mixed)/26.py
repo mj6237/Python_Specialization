c2ee1217fede90cbf10d1fbd9926118d190232df
Muhammad Jamshaid <128154619+mj6237@users.noreply.github.com>
2025-07-29 06:40:00 +0500

    How do you slice to the end of a list?

---
+++ Pyhton Challenges & Projects (Revising first 10 Chapters)/Practice Challenges/Section_6 (Mixed)/26.py
@@ -0,0 +1,9 @@
+'''
+1. Define a list: numbers = [10, 20, 30, 40, 50, 60, 70]. 
+2. Print a slice containing the second, third, and fourth elements. 
+3. Print a slice containing elements from the third element to the end. 
+'''
+numbers = [10, 20, 30, 40, 50, 60, 70]
+
+print(numbers[1:4])
+print(numbers[2:])
