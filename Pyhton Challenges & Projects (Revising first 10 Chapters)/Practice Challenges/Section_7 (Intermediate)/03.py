9b5ec4ebf44c3914867b8767489ae63e12341af9
Muhammad Jamshaid <128154619+mj6237@users.noreply.github.com>
2025-07-29 20:34:28 +0500

    Demonstrate tuple immutability with TypeError

---
+++ Pyhton Challenges & Projects (Revising first 10 Chapters)/Practice Challenges/Section_7 (Intermediate)/03.py
@@ -0,0 +1,11 @@
+'''
+1. Create a tuple named my_tuple = (10, 20, 30). 
+2. Try to change the first element of my_tuple to 100 (e.g., my_tuple[0] = 100). 
+3. Observe the error message that Python provides.
+'''
+
+my_tuple = (10, 20, 30)
+
+my_tuple[0] = 100
+
+print(my_tuple)
