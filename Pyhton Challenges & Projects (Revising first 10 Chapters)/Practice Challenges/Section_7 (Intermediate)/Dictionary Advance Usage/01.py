02eccb532ce0e076d3d196b59ff0dd6799f0b8ae
Muhammad Jamshaid <128154619+mj6237@users.noreply.github.com>
2025-07-29 22:32:11 +0500

    Iterate and print all keys from a dictionary.

---
+++ Pyhton Challenges & Projects (Revising first 10 Chapters)/Practice Challenges/Section_7 (Intermediate)/Dictionary Advance Usage/01.py
@@ -0,0 +1,9 @@
+''''
+1. Create a dictionary: student_scores = {"Alice": 95, "Bob": 88, "Charlie": 72}. 
+2. Use a for loop to iterate through and print only the names (keys) of the students. 
+'''
+
+student_scores = {"Alice": 95, "Bob": 88, "Charlie": 72}
+
+for names in student_scores :
+    print(names)
