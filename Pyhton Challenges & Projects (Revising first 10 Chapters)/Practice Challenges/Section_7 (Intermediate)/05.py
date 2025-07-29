e9bf1513b304eb9afa7dcfb0425ec9604a1732d4
Muhammad Jamshaid <128154619+mj6237@users.noreply.github.com>
2025-07-29 20:41:34 +0500

    Implement tuple concatenation using the '+' operator.

---
+++ Pyhton Challenges & Projects (Revising first 10 Chapters)/Practice Challenges/Section_7 (Intermediate)/05.py
@@ -0,0 +1,10 @@
+'''
+1. Create two tuples: tuple1 = (1, 2, 3) and tuple2 = (4, 5, 6). 
+2. Concatenate tuple1 and tuple2 to form a new_tuple. 
+3. Print new_tuple. 
+'''
+
+tuple1 = (1, 2, 3)
+tuple2 = (4, 5, 6)
+new_tuple = tuple1 + tuple2
+print(new_tuple)
