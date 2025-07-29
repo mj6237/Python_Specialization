7d4a18ff14db49874a51273376e4b889229f3a4f
Muhammad Jamshaid <128154619+mj6237@users.noreply.github.com>
2025-07-29 22:23:37 +0500

    Iterate through elements of a tuple using a for loop.

---
+++ Pyhton Challenges & Projects (Revising first 10 Chapters)/Practice Challenges/Section_7 (Intermediate)/09.py
@@ -0,0 +1,9 @@
+'''
+1. Create a tuple: weekdays = ("Mon", "Tue", "Wed", "Thu", "Fri"). 
+2. Use a for loop to print each day of the week from the tuple. 
+'''
+
+weekdays = ("Mon", "Tue", "Wed", "Thu", "Fri")
+
+for day in weekdays :
+    print(day)
