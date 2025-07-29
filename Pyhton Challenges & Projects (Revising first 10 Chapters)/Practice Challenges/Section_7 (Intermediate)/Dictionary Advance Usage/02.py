52902ccb832e1f709386f6fa4e6e1d22ea62eff0
Muhammad Jamshaid <128154619+mj6237@users.noreply.github.com>
2025-07-29 22:34:21 +0500

    tIerate and print all values from a dictionary

---
+++ Pyhton Challenges & Projects (Revising first 10 Chapters)/Practice Challenges/Section_7 (Intermediate)/Dictionary Advance Usage/02.py
@@ -0,0 +1,8 @@
+'''
+1. Use the student_scores dictionary from Problem 2.1. 
+2. Use a for loop to iterate through and print only the scores (values) of the students. 
+'''
+student_scores = {"Alice": 95, "Bob": 88, "Charlie": 72}
+
+for values in student_scores.values() :
+    print(values)
