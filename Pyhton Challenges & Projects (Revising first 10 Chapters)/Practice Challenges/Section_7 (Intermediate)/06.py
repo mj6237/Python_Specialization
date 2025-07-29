9a749a0c819d2489c386c69ed26d34d5600b806c
Muhammad Jamshaid <128154619+mj6237@users.noreply.github.com>
2025-07-29 20:48:48 +0500

    Implement checking for item existence within a tuple.

---
+++ Pyhton Challenges & Projects (Revising first 10 Chapters)/Practice Challenges/Section_7 (Intermediate)/06.py
@@ -0,0 +1,17 @@
+'''
+1. Create a tuple: colors = ("red", "green", "blue"). 
+2. Check if "green" is in the colors tuple. Print "Green is in the tuple." or "Green is NOT in the tuple." 
+accordingly. 
+3. Check if "yellow" is in the colors tuple. Print "Yellow is in the tuple." or "Yellow is NOT in the tuple." 
+accordingly. 
+'''
+colors = ("red", "green", "blue")
+
+if "green" in colors :
+    print("Green is in the tuple.")
+else :
+    print("Green is NOT in tuple.")
+if "yellow" in colors :
+    print("Yellow is in the tuple.")
+else :
+    print("Yellow is NOT in tuple.")
