bbaa0673aeb520e922e9e89052c3eaa4e54be202
Muhammad Jamshaid <128154619+mj6237@users.noreply.github.com>
2025-07-29 22:27:14 +0500

    Convert a list to a tuple using the tuple() constructor

---
+++ Pyhton Challenges & Projects (Revising first 10 Chapters)/Practice Challenges/Section_7 (Intermediate)/10.py
@@ -0,0 +1,10 @@
+'''
+1. Create a list: my_list = [1, 2, 3, 4, 5]. 
+2. Convert my_list into a tuple named my_tuple. 
+3. Print my_tuple and its type().
+'''
+my_list = [1, 2, 3, 4, 5]
+
+my_tuple = tuple(my_list)
+print(my_tuple)
+print(type(my_tuple))
