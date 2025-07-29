0429b759a804a9d9c6c693965356d523a97e89a5
Muhammad Jamshaid <128154619+mj6237@users.noreply.github.com>
2025-07-29 20:29:36 +0500

    Single-Element Tuple creation and type check.

---
+++ Pyhton Challenges & Projects (Revising first 10 Chapters)/Practice Challenges/Section_7 (Intermediate)/02.py
@@ -0,0 +1,16 @@
+'''
+1. Create a tuple named single_item_tuple containing only the integer 5. 
+2. Print the tuple. 
+3. Print the type() of single_item_tuple. 
+4. Create a variable not_a_tuple by putting (5) without a comma. 
+5. Print the type() of not_a_tuple to see the difference. 
+ Simple Hints: 
+o What special syntax is required for a single-element tuple? 
+'''
+
+single_item_tuple = (5,)
+print(single_item_tuple)
+print(type(single_item_tuple))
+
+not_a_tuple = (5)
+print(type(not_a_tuple))
