792a5b31bb915a5b3008a21e7e487ad7a3f5e54a
Muhammad Jamshaid <128154619+mj6237@users.noreply.github.com>
2025-07-29 20:29:05 +0500

    Add RGB color tuple and print Green component

---
+++ Pyhton Challenges & Projects (Revising first 10 Chapters)/Practice Challenges/Section_7 (Intermediate)/01.py
@@ -0,0 +1,14 @@
+'''
+ What the Question Demands: 
+1. Create a tuple named rgb_color with three integer values representing Red, Green, and Blue (e.g., (255, 0, 
+0) for red). 
+2. Print the entire rgb_color tuple. 
+3. Print the value of the second element (Green).
+'''
+
+rgb_color = (255, 0, 0)
+#print(type(rgb_color))
+print(rgb_color)
+print(rgb_color[1])
+
+
