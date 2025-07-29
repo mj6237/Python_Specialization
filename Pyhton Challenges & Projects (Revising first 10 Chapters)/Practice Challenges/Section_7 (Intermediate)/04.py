9f6affa083e4c6ba6b79815468f9dd24bb892823
Muhammad Jamshaid <128154619+mj6237@users.noreply.github.com>
2025-07-29 20:38:55 +0500

    Implement tuple slicing for extracting sub-sequences

---
+++ Pyhton Challenges & Projects (Revising first 10 Chapters)/Practice Challenges/Section_7 (Intermediate)/04.py
@@ -0,0 +1,9 @@
+'''
+1. Create a tuple named alphabet_tuple = ('a', 'b', 'c', 'd', 'e', 'f'). 
+2. Print a slice containing the elements from 'b' to 'd' (inclusive). 
+3. Print a slice containing elements from 'd' to the end. 
+'''
+alphabet_tuple = ('a', 'b', 'c', 'd', 'e', 'f')
+print(alphabet_tuple[1:4])
+
+print(alphabet_tuple[3:])
