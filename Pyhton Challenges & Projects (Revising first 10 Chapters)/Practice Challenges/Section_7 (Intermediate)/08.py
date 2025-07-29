7df697aed63a43cdb4c949b0d34434edf6cf468b
Muhammad Jamshaid <128154619+mj6237@users.noreply.github.com>
2025-07-29 22:21:30 +0500

    Implement finding item index in tuple and demonstrate ValueError for missing item.

---
+++ Pyhton Challenges & Projects (Revising first 10 Chapters)/Practice Challenges/Section_7 (Intermediate)/08.py
@@ -0,0 +1,10 @@
+'''
+1. Create a tuple: planets = ("Mercury", "Venus", "Earth", "Mars", "Jupiter"). 
+2. Find the index of "Earth". 
+3. Print the index. 
+4. Try to find the index of "Pluto" and observe the error.
+'''
+planets = ("Mercury", "Venus", "Earth", "Mars", "Jupiter")
+
+print(planets.index("Earth"))
+print(planets.index("Pluto"))
