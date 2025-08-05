36491d17477c7673f0e3b727f75a0ffb06b886a5
Muhammad Jamshaid <128154619+mj6237@users.noreply.github.com>
2025-08-05 18:26:35 +0500

    Creating class

---
+++ Chapter_14 (OOP)/CreatingClass.py
@@ -0,0 +1,12 @@
+# Creating Class
+
+class Student :
+    name = "Bilal"
+    roll_no = 45
+    degree = "BSCS"
+
+# Creating objects
+
+student1 = Student() #  <---  Object created here
+print(student1.name)
+
